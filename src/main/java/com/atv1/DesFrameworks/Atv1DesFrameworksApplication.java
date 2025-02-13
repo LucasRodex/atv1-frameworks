abd99dc7e1b16d3990eeb31d1ab506b95d28d539
lucas <lucascarlosrodex@gmail.com>
2025-02-12 22:48:38 -0300

    first commit

---
+++ src/main/java/com/atv1/DesFrameworks/Atv1DesFrameworksApplication.java
@@ -0,0 +1,13 @@
+package com.atv1.DesFrameworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atv1DesFrameworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atv1DesFrameworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atv1/DesFrameworks/Atv1DesFrameworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atv1.DesFrameworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atv1DesFrameworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
